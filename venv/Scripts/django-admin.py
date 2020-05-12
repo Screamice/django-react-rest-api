ba718514abfecd510266ef45915c17aa5b034f60
Misael Rico <misaelrico786@gmail.com>
2020-05-12 14:45:19 -0500

    First commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\misae\documents\proyectos personales\django-react-rest-api\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
